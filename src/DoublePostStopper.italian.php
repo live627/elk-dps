755a71e09cbad22c896751933af37d3a60632747
radu81 <radu_81@hotmail.com>
2018-11-10 00:57:03 +0100

    Update DoublePostStopper.italian.php

--- src/DoublePostStopper.italian.php
+++ src/DoublePostStopper.italian.php
@@ -10,8 +10,6 @@
 
 global $helptxt;
 
-$txt['double_post_attempt'] =
-    'Spiacenti, non è possibile pubblicare messaggi consecutivi. Torna indietro e modifica il messaggio precedente.';
-$txt['doublePostThresholds'] = 'Intervallo entro cui l'utente non è autorizzato a creare doppi post';
-$txt['doublePostThresholds_desc'] =
-    'A user is not allowed to make two or more consecutive posts within a given time period. The applicable group threshold is the lowest in the user\'s groups.<br><br>Example: Global moderators get 1 hour while newbies have 3 days to wait. If a newbie is also a global moderator, then their wait time is 1 hour.';
+$txt['double_post_attempt'] = 'Spiacenti, non è possibile pubblicare messaggi consecutivi. Torna indietro e modifica il messaggio precedente.';
+$txt['doublePostThresholds'] = 'Intervallo entro cui l\'utente non è autorizzato a creare doppi post';
+$txt['doublePostThresholds_desc'] = 'A user is not allowed to make two or more consecutive posts within a given time period. The applicable group threshold is the lowest in the user\'s groups.<br><br>Example: Global moderators get 1 hour while newbies have 3 days to wait. If a newbie is also a global moderator, then their wait time is 1 hour.';
